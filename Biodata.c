78d57ed379dad78dd2c73876e9eb8cac04fc203e
rifkirizaalfiansyah <19081010105@student.upnjatim.ac.id>
2020-09-16 12:10:04 +0700

    First commit

---
+++ Biodata.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	printf("===============Biodata Pribadi===================\n");
+	printf("Nama\t: Rifki Riza Alfiansyah\n");
+	printf("NPM\t: 19081010105\n");
+	printf("Status\t: Mahasiswa\n");
+	printf("Sekolah\t: UPN Veteran Jawa Timur\n");
+	printf("Alamat\t: Kecamatan Bugul Kidul, Pasuruan\n");
+	printf("No HP\t: 089687732941");
+	
+	return 0;
+}
